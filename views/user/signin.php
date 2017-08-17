6b00ca35f365db1db1108e1b7fe54e656959d935
Tim Nazar <timnazar@live.com>
2017-08-17 16:32:02 -0700

    added input fields

--- views/user/signin.php
+++ views/user/signin.php
@@ -5,7 +5,10 @@
     <section id="sign-up-content">
         <label>Log in</label>
         <form action="#" method="POST">
-           
+           <input type="email" placeholder="Email" name="email">
+           <input type="password" placeholder="Password" name="password">
+
+           <input type="submit" value="Sign in">
         </form>
     </section>
 
