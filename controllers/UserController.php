7b412f2f8a7d6d3e07a818b64b992d37207cd8e8
Manuchekhr <manucher5160@gmail.com>
2017-08-17 10:57:19 +0300

    48512

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -6,32 +6,23 @@ class UserController
 		//Registration for applicants
 
 		public function actionSignup()
-<<<<<<< HEAD
 		{
 			if($_POST){
-				if($_POST['actionSignup']==1){
-=======
-		{if($_POST){
->>>>>>> 5fa6a9e908add7e020249ad0fb35875384b81cf9
+			if($_POST['actionSignup']){
 			$firstname=$_POST['firstname'];
 			echo $firstname;
-			$secondname=$_POST['secondname'];	
-			echo '<br/>'.$secondname;
+			$lastname=$_POST['lastname'];	
+			echo '<br/>'.$lastname;
 			$password=$_POST['password'];	
 			echo '<br/>'.$password;
 			$email=$_POST['email'];	
 			echo '<br/>'.$email;
-<<<<<<< HEAD
 			$cellphone=$_POST['cellphone'];
 			echo $cellphone;}
-			else if ($_POST['actionEmployer']==2)
+			else if ($_POST['actionEmployer'])
 			
 			{ echo 'trfgh';
 			}
-=======
-			$check=$_POST['a'];
-			echo $check;
->>>>>>> 5fa6a9e908add7e020249ad0fb35875384b81cf9
 			
 		}
 			require_once(ROOT . '/views/user/signup.php'); 
@@ -41,23 +32,8 @@ class UserController
 
 		//Registration for employeers
 		
-<<<<<<< HEAD
 	
-=======
-		public function actionEmployer()
-		{
-			return true;
-		}
-		
->>>>>>> 5fa6a9e908add7e020249ad0fb35875384b81cf9
 
-		//Login
-
-		public function actionLogin()
-		{
-
-
-		}
 
 
 }
