3430b4a553b22f0fd58342cb69083bcf47bbdd87
Tim Nazar <timnazar@live.com>
2017-08-17 14:50:32 -0700

    added signin view

---
+++ views/user/signin.php
@@ -0,0 +1,15 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+
+<div id="wrapper">
+    <section id="sign-up-content">
+        <label>Log in</label>
+        <form action="#" method="POST">
+           
+        </form>
+    </section>
+
+
+</div>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
