9689a6b1da5cf83e6ab40f23a0efb3fdb378d855
Manuchekhr <manucher5160@gmail.com>
2017-08-18 14:22:59 +0300

    tags

--- controllers/TagController.php
+++ controllers/TagController.php
@@ -6,10 +6,8 @@ class TagController
 
 		public function actionIndex()
 		{ 
-			$uri = Router::getURI();
-			$internalRoute=preg_replace('~tag/~','',$uri);
-			echo $internalRoute;
- 			require_once (ROOT. '/views/tag/tags.php');
+			 require_once (ROOT. '/views/tag/tags.php');
+			 require_once(ROOT.'/models/Tags.php');
 		}
 	
 
