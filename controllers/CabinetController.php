9c4cfceed5d127a6e43ddb2f47ba58ff4233fda1
Javhar Ziyoev <javhar.ziyoev@gmail.com>
2017-08-20 22:12:41 +0500

    mm

--- controllers/CabinetController.php
+++ controllers/CabinetController.php
@@ -6,10 +6,10 @@ class CabinetController
     public function actionIndex()
     {
         $userId = User::checkLogged();
-        echo $userId;
+
         //get info about user from DB
         $user = User::getUserById($userId);
-        echo $user;
+
         
         require_once(ROOT . '/views/cabinet/index.php');
         return true;
