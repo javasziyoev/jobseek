b56072c9268d2b8ee69d10c9ae1c5d5a4686b405
Javhar Ziyoev <javhar.ziyoev@gmail.com>
2017-08-21 22:27:00 +0500

    news

--- config/routes.php
+++ config/routes.php
@@ -16,6 +16,7 @@ return array(
 	'panel/moder' => 'panel/moder',
 	'panel/admin' => 'panel/admin',
 	'panel/applicant' => 'panel/applicant',
-	'news/([0-9]+)' => 'news/view/$1',
+	'news' => 'news/index',
+	'news/id/([0-9]+)' => 'news/view/$1',
 	
 	);
--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,6 +1,10 @@
 <?php
 class NewsController{
 
+public function actionIndex(){
+    require_once(ROOT.'/views/news/index.php');
+}
+
 public function actionView($category){
     echo $category;
 }
