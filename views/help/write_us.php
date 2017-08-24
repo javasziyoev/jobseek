12b018ff3eff665d3d1c5baa0217404a23e4c112
Javhar Ziyoev <javhar.ziyoev@gmail.com>
2017-08-24 21:22:52 +0500

    dd

--- views/help/write_us.php
+++ views/help/write_us.php
@@ -6,7 +6,7 @@ echo
 '<div id="wrapper">
 <section id="vacancy-details-content">
 <h1 class="dark-gray-text">WRITE TO US</h1>
-<input type="text" class="input-text" name="helpinput" placeholder="email"></input><br>
+<input type="text" class="input-text" name="helpinput" placeholder="email"></input><br><br>
 <textarea name="helptext"></textarea>
 
 <div class="vacancy-info"></div>
