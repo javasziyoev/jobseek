5f7d6ef369c7f01bd7327882664333ad4c5860f0
Tim Nazar <timnazar@live.com>
2017-08-25 11:33:33 +0500

    мозилла лучше

--- views/employer/post_a_vacancy.php
+++ views/employer/post_a_vacancy.php
@@ -1,14 +1,15 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
 
 <div id="wrapper">
+<section id="sign-up-content">
   <div id="inner-content-thin">
       <section id="post-a-vacancy-content">
-        <h3 class="dark-gray-text">Post a vacancy</h3>
+        <h3 class="dark-gray-text" style="text-align: center; margin-bottom: 1em;">Post a vacancy</h3>
         <div>
           <form action="" method="POST">
-            <input name ="position" type="text" name="position" placeholder="Position" class="input-text"><br>
-            <input name ="salary"   type="text" name="salary" placeholder="Salary" class="input-text">
-              <select name ='curency'  class="select-category">
+            <input name ="position" type="text" name="position" placeholder="Position" class="input-text mob-button-100 wid-100"><br>
+            <input name ="salary"   type="text" name="salary" placeholder="Salary" class="input-text mob-button-100 wid-100">
+              <select name ='curency'  class="select-category mob-button-100 wid-100">
                  <?php 
           
                 
@@ -21,9 +22,9 @@ echo'<option value='.$current['currency_id'].'>'.$current['currency_code'].'</op
                  ?>
                   
               </select><br>
-            <input name ="required_experience" type="text" name="salary" placeholder="Required experience" class="input-text"><br>
-            <p>Select industry: 
-            <select name = 'industry'class="select-category">
+            <input name ="required_experience" type="text" name="salary" placeholder="Required experience" class="input-text mob-button-100 wid-100"><br>
+            <label style="margin-left: .3em;"><strong>Select industry:</strong></label>
+            <select name = 'industry'class="select-category wid-100">
                
             <?php
             
@@ -34,9 +35,9 @@ echo'<option value='.$current['currency_id'].'>'.$current['currency_code'].'</op
 echo'<option value='.$current['industry_id'].'>'.$current['industry_name'].'</option>';
              }
              ?>
-            </select></p>
-            <p>Employment type: 
-            <select name ="employment_type" class="select-category">
+            </select>
+            <label style="margin-left: .3em;"><strong>Employment type:</strong></label>
+            <select name ="employment_type" class="select-category mob-button-100 wid-100">
             <?php
             
            
@@ -46,8 +47,8 @@ echo'<option value='.$current['industry_id'].'>'.$current['industry_name'].'</op
 echo'<option value='.$current['employment_type_id'].'>'.$current['employment_type_name'].'</option>';
              }
              ?>
-            </select></p>
-            <p>City: <select name = "city" class="select-category">
+            </select>
+            <label style="margin-left: .3em;"><strong>City:</strong></label><select name = "city" class="select-category mob-button-100 wid-100">
             <?php
             
             
@@ -56,20 +57,19 @@ echo'<option value='.$current['employment_type_id'].'>'.$current['employment_typ
              {
 echo'<option value='.$current['city_id'].'>'.$current['city_name'].'</option>';
              }
-             ?>            </select></p>
+             ?>            </select>
 
-            <p>Address:
-            
-             <input name = "address" type="text" class="input-text"></p>
-             <textarea name = "short" placeholder="Short Description" class="input-text short-descr"></textarea><br>
+             <input name = "address" type="text" class="input-text mob-button-100 wid-100" placeholder="Address">
+             <textarea name = "short" placeholder="Short Description" class="input-text short-descr mob-button-100 wid-100"></textarea><br>
 
-            <textarea name = "info" placeholder="Info" class="input-text descr"></textarea><br>
+            <textarea name = "info" placeholder="Info" class="input-text descr mob-button-100 wid-100"></textarea><br>
 
-            <input name  = "submit" type="submit" value="Post a vacancy" class="post-button" style="width: 150px;">
+            <input name  = "submit" type="submit" value="Post a vacancy" class="post-button mob-button-100 wid-100" >
           </form>
         </div>
       </section>
     </div>
+            </section>
             </div>
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
