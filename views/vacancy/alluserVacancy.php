47e01b1a607a8094d4b3fdaceb385c292a233104
Tim Nazar <timnazar@live.com>
2017-08-25 12:13:55 +0500

    asdaf

--- views/vacancy/alluserVacancy.php
+++ views/vacancy/alluserVacancy.php
@@ -1,11 +1,66 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
 
-<div id="wrapper" style="margin-top: 25px">
 
 
-          <!-- ТИмур запили нормальный вывод здесь , спасибо -->
-  <section id="main-content news" style="padding: .5% 2%; background-color: #fafafa;">
-  <h3 class="dark-gray-text">All vacancies by     <?php
+
+<div id="wrapper">
+      <section id="top-jobs-content">
+          
+          <section id="jobs-at-content">
+            <div style="text-align: center; width: 90%; height: 200px; background-color: orange; margin-left: 3%; margin-right: 3%; margin-bottom: 1em;">
+              For small add
+            </div>
+              <h3 class="dark-gray-text">Jobs at</h3>
+              <div>
+                  <ul>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Microsoft</a></span>
+                          <span class="jobs-count">42</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Google</a></span>
+                          <span class="jobs-count">5</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">China Bank</a></span>
+                          <span class="jobs-count">7</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Union Pay</a></span>
+                          <span class="jobs-count">2</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">NVIDIA</a></span>
+                          <span class="jobs-count">10</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Xiaomi</a></span>
+                          <span class="jobs-count">36</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Meizu</a></span>
+                          <span class="jobs-count">15</span>
+                        </div>
+                      </li>
+                  </ul>
+              </div>  
+          </section>
+           
+          <section id="featured-jobs-content">
+          <h1 class="dark-gray-text">All vacancies by     <?php
   
            $db = Db::getConnection();
            
@@ -16,9 +71,9 @@ $result=$db->prepare($sql);
 $result->execute();
 $user = $result->fetch();
 echo $user['company_name'];  
-?></h3>
+?></h1>
 <div id="inner-content news-content" style=" margin-left:auto; margin-right:auto;" class="width-900">
-        <ul class="all-user-vacancies-ul" style="display: block; margin-top: 2em;">
+        <ul class="all-user-vacancies-ul" style="display: block; margin-top: 1em;">
         <?php            
         
            $details =  Details::tagSearch();
@@ -55,12 +110,23 @@ echo $user['company_name'];
         
         <br style="clear:both">
       </div>    
-</section>
+          
 
 
 
 
- </div>
+          </section>
+
+
+          <section id="reklama-bleat">
+          Отключите блокировщик рекламы плес.
+          </section>
+      </section>
+
+
+</div>
+
+
 
 
 
