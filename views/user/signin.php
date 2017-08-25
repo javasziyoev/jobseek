f2214469a53f28aaabeabcc740eaa50a32c1e2b0
Tim Nazar <timnazar@live.com>
2017-08-25 10:17:12 +0500

    asd

--- views/user/signin.php
+++ views/user/signin.php
@@ -2,16 +2,17 @@
 
 
 <div id="wrapper">
-    <section id="sign-up-content">
-        <p>Sign in to Job Seeker</p>
-        <form action="#" method="POST">
-           <input type="text" placeholder="Email" name="loginemail" class="input-text" ><br>
-           <input type="password" placeholder="Password" name="loginpassword" class="input-text" ><br><br>
-           <input type="submit" name="loginsubmit" value="Sign in" class="sign-in-button">
+    <div class="mobwrapper">
+    <section id="sign-in-content" >
+        <h1 style="margin-bottom: 1em; text-align: center;">Sign in to Job Seeker</h1>
+        <form action="#" method="POST" style="width: 100%;">
+           <input type="text" placeholder="Email" name="loginemail" class="input-text mob-button-100" ><br>
+           <input type="password" placeholder="Password" name="loginpassword" class="input-text mob-button-100" ><br><br>
+           <input type="submit" name="loginsubmit" value="Sign in" class="sign-in-button mob-button-100" style="width: 100%;">
         </form>
     </section>
-  <div> Dont have an account? <a href = "/user/signup" >Create one </a></div>
-
+  <div style="margin-bottom: 10em; text-align: center;"> Dont have an account? <a href = "/user/signup" >Create one </a></div>
+</div>
 </div>
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
