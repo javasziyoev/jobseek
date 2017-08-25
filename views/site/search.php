7d2aeccac3fc531469398073249c076effe79e08
Tim Nazar <timnazar@live.com>
2017-08-25 11:40:50 +0500

    asd

--- views/site/search.php
+++ views/site/search.php
@@ -1,11 +1,13 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
 
+<div id="wrapper">
+    <section id="sign-up-content">
 
 
 
 
-
-
+    </section>
+</div>
 
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
