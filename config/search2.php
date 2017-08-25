abf1489cf338ab699ab79c716ca4621e84ed2a2a
Javhar Ziyoev <javhar.ziyoev@gmail.com>
2017-08-26 00:12:07 +0500

    ss

--- config/search2.php
+++ config/search2.php
@@ -4,16 +4,16 @@
        echo $res[$i][2]; $i++;}
     echo '<div class="vacancy-post">
         <div class="vacancy-postposted">
-            <a href="" class="vacancy-title">'.$position.'</a>
-            <div class="salary"><label>'.$salary.'</label>
-            <label style="margin-left: 5px;">'.$currency.'</label></div>
-            <div class="short-description">'.$short.'</div>
-            <a href="" class="company-name">'.$res[$i][2].'</a>
+            <a href="" class="vacancy-title"></a>
+            <div class="salary"><label></label>
+            <label style="margin-left: 5px;"></label></div>
+            <div class="short-description"></div>
+            <a href="" class="company-name"></a>
             <div> 
                 <span>
-                    <label class="city">'.$city_name.'</label><br/>
-                    <label class="post-date">'.$date.'</label> <br/>
-                    <label class="jobid">'.$job.'</label>
+                    <label class="city"></label><br/>
+                    <label class="post-date"></label> <br/>
+                    <label class="jobid"></label>
                 </span>
             </div>
         </div>
