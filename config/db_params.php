083a71770bbdfcaa611201d21591097e2929597c
Manuchekhr <manucher5160@gmail.com>
2017-08-30 16:17:32 +0300

    4454

--- config/db_params.php
+++ config/db_params.php
@@ -1,19 +1,19 @@
 <?php
-
+/*
 return array(
 			'host' => 'localhost',
 			'dbname' => 'jobseeker',
 			'user' => 'root',
 			'password' => '',
-);
+);*/
 
 
-/*
+
 return array(
 			'host' => '127.0.0.1:53767',
 			'dbname' => 'localdb',
 			'user' => 'azure',
 			'password' => '6#vWHD_$',
-);*/
+);
 
 ?>
