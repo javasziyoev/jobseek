1e6121f23c570776aef885beed964c4d331e72ad
Tim Nazar <timnazar@live.com>
2017-08-31 06:50:40 -0700

    asd

--- views/site/search.php
+++ views/site/search.php
@@ -5,6 +5,65 @@
  </head>
 
 <div id="wrapper">
+
+<section id="top-jobs-content">
+          
+          <section id="jobs-at-content">
+            <div style="text-align: center; width: 90%; height: 200px; background-color: orange; margin-left: 3%; margin-right: 3%; margin-bottom: 1em;">
+              For small add
+            </div>
+              <h3 class="dark-gray-text">Jobs at</h3>
+              <div>
+                  <ul>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Microsoft</a></span>
+                          <span class="jobs-count">42</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Google</a></span>
+                          <span class="jobs-count">5</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">China Bank</a></span>
+                          <span class="jobs-count">7</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Union Pay</a></span>
+                          <span class="jobs-count">2</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">NVIDIA</a></span>
+                          <span class="jobs-count">10</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Xiaomi</a></span>
+                          <span class="jobs-count">36</span>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <span><a href="" class="dark-gray-text">Meizu</a></span>
+                          <span class="jobs-count">15</span>
+                        </div>
+                      </li>
+                  </ul>
+              </div>  
+          </section>
+           
+          <section id="vacancy-details-content" style="margin-left: 1em;">
+
+
     <section id="sign-up-content">
     <?php 
 
@@ -85,7 +144,13 @@ if($res){
     }
 }
         ?>
+ </section>
 
+
+          <section id="reklama-bleat">
+          Отключите блокировщик рекламы плес.
+          </section>
+      </section>
 </div>
 </form>
 
