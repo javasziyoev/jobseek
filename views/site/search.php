55e9b81f57a37bf7c810821db862e667cb2dff36
Javhar Ziyoev <javhar.ziyoev@gmail.com>
2017-09-01 20:01:29 +0500

    nn

--- views/site/search.php
+++ views/site/search.php
@@ -86,7 +86,7 @@ if($res){
     {
       
         //city
-        print_r($res[$i][7]);
+  
         $city = Search::getCityById($res[$i][7]);
         //  
         //employer
@@ -111,7 +111,7 @@ if($res){
             <a href="/vacancy/all/'.$employer[$i][0].'" class="company-name">'.$employer[2].'</a>
             <div> 
                 <span>
-                    <label class="city company-name">'.$city.'</label>
+                    <label class="city company-name">'.$city['city_name'].'</label>
                     <label class="post-date company-name">'.$res[$i][8].'</label> <br/>
                     <label class="jobid company-name">'.$job.'</label>
                 </span>
