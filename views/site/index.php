06ec4630d34a1f63932bf4a10e448f333bf2ac11
Tim Nazar <timnazar@live.com>
2017-09-09 14:53:14 +0500

    ;fdfc negjq ujyljy

--- views/site/index.php
+++ views/site/index.php
@@ -144,7 +144,50 @@
               </div>  
           </section>
            
-         
+          <section id="featured-jobs-content">
+            <h3 class="dark-gray-text">Featured jobs</h3>
+            <div class="vacancies-of-the-day" id="inner-content" style="padding-left: 0px;">
+                      <?php            
+               
+                $getFJobId = User::getFJobId();
+                $db = Db::getConnection();
+                
+                foreach($getFJobId as $getJobs)
+                {
+                 
+                 echo '              <div class="vacancies-of-the-day-item ">
+                <a href="/vacancy/details/'.$getJobs['vacancy_id'].'">
+';                echo ' <span class="vacancy-of-the-day-title">';
+                 echo $getJobs['position'].'</span>';
+                  echo '   <span class="vacancy-of-the-day-salary">';
+                  echo $getJobs['salary'].' ';
+                  $sql = 'SELECT * FROM `currency` WHERE currency_id='.$getJobs['salary_currency_id'];
+                  $result=$db->prepare($sql);
+                  $result->execute();
+                  $user = $result->fetch();
+     
+                  echo $user['currency_code'].'</span>';           
+                     
+                 $sql = 'SELECT * FROM `employer` WHERE employer_id ='.$getJobs['employer_id'];
+                 $result = $db->prepare($sql);
+                 $result->execute();
+                 $job=$result->fetch();
+                 echo '<a href="/vacancy/all/'.$getJobs['employer_id'].'"><span class="vacancy-of-the-day-company">';
+                 echo $job['company_name'];
+                 echo '</span></a></a>
+                 </div>';
+                }        
+                   ?>
+                
+              
+            </div>  
+          </section>
+
+
+          <section id="reklama-bleat">
+          Отключите блокировщик рекламы плес.
+          </section>
+      </section>
 
       <section id="mobad" style="padding: .5% 2%; background-color: orange; height: 200px;">
         <div id="inner-content news-content" style=" margin-left:auto; margin-right:auto; text-align: center;" class="width-900">
