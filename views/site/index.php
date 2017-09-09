7ae5c4f1cc1b24658fa83dac4fc9b9f0091085d8
Tim Nazar <timnazar@live.com>
2017-09-09 14:47:46 +0500

    жавас даун

--- views/site/index.php
+++ views/site/index.php
@@ -25,7 +25,44 @@
 </section>
 
 
-
+<section id="main-content news" style="padding: .5% 2%; background-color: #fafafa;">
+        <div id="inner-content news-content" style=" margin-left:auto; margin-right:auto; text-align: center; margin: 0 auto;
+    width: 100%;" class="width-900">
+          <ul class="work-in-industry-ul" style="display: inline-flex; margin-top: 0;">
+            <div id="newsy" class="desnewsy mobnewsy">
+            <?php 
+            $c = News::getAmount();
+            $c = $c['COUNT(*)'];
+            $q = News::getView($c);
+            $w = News::getView($c-1);
+            $e = News::getView($c-2);
+            $r = News::getView($c-3);
+            echo
+             ' <div><strong><a href="news/view/'.$c.'">'.$q[0][1].'</a></strong></div>
+              <div>'.$q[0][2].'</div>
+            </div>
+            
+            <div id="newsy" class="desnewsy mobnewsy">
+              <div><strong><a href="news/view/'.($c-1).'">'.$w[0][1].'а</a></strong></div>
+              <div>'.$w[0][2].'</div>
+            </div>
+            
+            <div id="newsy" class="desnewsy mobnewsy">
+              <div><strong><a href="news/view/'.($c-2).'">'.$e[0][1].'</a></strong></div>
+              <div>'.$e[0][2].'</div>
+            </div>
+            
+            <div id="newsy" class="desnewsy mobnewsy" style="border-color: #fafafa;">
+              <div><strong><a href="news/view/'.($c-3).'">'.$r[0][1].'</a></strong></div>
+              <div>'.$r[0][2].'</div>
+            </div>';
+            ?>
+          </ul>
+          
+          <br style="clear:both" />
+        </div>    
+          
+      </section>
 
       <section id="mobad" style="padding: .5% 2%; background-color: orange;">
         <div id="inner-content news-content" style=" margin-left:auto; margin-right:auto; text-align: center;" class="width-900">
@@ -44,10 +81,9 @@
             <div style="text-align: center; width: 90%; height: 200px; background-color: orange; margin-left: 3%; margin-right: 3%; margin-bottom: 1em;">
               For small add
             </div>
-              <h3 class="dark-gray-text">Jobs At Provinces</h3>
+              <h3 class="dark-gray-text">Jobs at</h3>
               <div>
                   <ul>
-                  
                       <li>
                         <div>
                           <span><a href="" class="dark-gray-text">Microsoft</a></span>
@@ -109,10 +145,38 @@
           </section>
            
           <section id="featured-jobs-content">
-            <h3 class="dark-gray-text">News</h3>
+            <h3 class="dark-gray-text">Featured jobs</h3>
             <div class="vacancies-of-the-day" id="inner-content" style="padding-left: 0px;">
                       <?php            
-              
+               
+                $getFJobId = User::getFJobId();
+                $db = Db::getConnection();
+                
+                foreach($getFJobId as $getJobs)
+                {
+                 
+                 echo '              <div class="vacancies-of-the-day-item ">
+                <a href="/vacancy/details/'.$getJobs['vacancy_id'].'">
+';                echo ' <span class="vacancy-of-the-day-title">';
+                 echo $getJobs['position'].'</span>';
+                  echo '   <span class="vacancy-of-the-day-salary">';
+                  echo $getJobs['salary'].' ';
+                  $sql = 'SELECT * FROM `currency` WHERE currency_id='.$getJobs['salary_currency_id'];
+                  $result=$db->prepare($sql);
+                  $result->execute();
+                  $user = $result->fetch();
+     
+                  echo $user['currency_code'].'</span>';           
+                     
+                 $sql = 'SELECT * FROM `employer` WHERE employer_id ='.$getJobs['employer_id'];
+                 $result = $db->prepare($sql);
+                 $result->execute();
+                 $job=$result->fetch();
+                 echo '<a href="/vacancy/all/'.$getJobs['employer_id'].'"><span class="vacancy-of-the-day-company">';
+                 echo $job['company_name'];
+                 echo '</span></a></a>
+                 </div>';
+                }        
                    ?>
                 
               
@@ -147,8 +211,6 @@
                 foreach($forsectorsId as $sectors)
                 {
                  echo '<li><a href= /tag/'.$sectors['industry_id'].'>';
-
-
                   echo ''.$forsectorsName[$i].'</a></li>';
                   $i++;
                 }        
