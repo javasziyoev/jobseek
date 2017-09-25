df5b7544d6b95dce5c2605455649cc7ed2c323dd
Javhar Ziyoev <javhar.ziyoev@gmail.com>
2017-09-25 23:16:12 +0500

    company

--- controllers/CompanyController.php
+++ controllers/CompanyController.php
@@ -5,13 +5,13 @@ class CompanyController
 
     public function actionAllComp($page)
     {
-        $userId = User::checkLogged();
+       
         $k = UserVacancs::getCompanyAmount();
         $cubes = 8;
         $e = floor($k / $cubes) + 1;
         $detail =  UserVacancs::getCompany($page,$cubes);
         //get info about user from DB
-        $user = User::getUserById($userId);
+        
         
         
         require_once(ROOT . '/views/site/company.php');
